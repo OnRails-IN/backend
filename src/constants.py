103f4c457151bc379044fe04ac37c90b48b000e3
datmemerboi <shravanssd@gmail.com>
2021-01-27 01:14:12 +0530

    Added new config constants

--- src/constants.py
+++ src/constants.py
@@ -6,5 +6,9 @@ ENV = os.environ.get("PYTHON_ENV")
 with open(os.path.join(os.path.dirname(__file__), 'config.yaml'), 'r') as file:
 	config = yaml.load(file, Loader = yaml.Loader)[ENV]
 
+SECRET_KEY = config['SECRET_KEY']
 DOMAIN = config['DOMAIN']
 ES_URI = config['ES_URI']
+REDIS_HOST = config['REDIS_HOST']
+REDIS_PORT = config['REDIS_PORT']
+REDIS_PASSWORD = config['REDIS_PASSWORD']
